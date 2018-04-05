a881e35ee3f1b66347eee5b5f4c0a1fd3808b623
Alexey Orlov <alexey@unity3d.com>
2018-04-05 10:49:23 +0300

    updating metal plugin sample to better show usage of all the api we provide

---
+++ Graphics/MetalNativeRenderingPlugin/Assets/TestPlugin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlugin : MonoBehaviour {
+	IEnumerator OnFrameEnd() {
+		yield return new WaitForEndOfFrame();
+		// note that we do that AFTER all unity rendering is done.
+		// it is especially important if AA is involved, as we will end encoder (resulting in AA resolve)
+		SamplePlugin.DoCopyRT(GetComponent<Camera>().targetTexture, null);
+		yield return null;
+	}
+
+	void OnPostRender() {
+		SamplePlugin.DoExtraDrawCall();
+		StartCoroutine(OnFrameEnd());
+	}
+}
